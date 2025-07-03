bce8f8ca9e5bac150b4219029e91289b2cf59c0d
raoabdulrafay05 <153934084+raoabdulrafay05@users.noreply.github.com>
2025-07-03 16:05:03 +0500

    Add files via upload

---
+++ config/tomato_classes.py
@@ -0,0 +1,12 @@
+CLASS_NAMES = [
+    'Tomato Bacterial Spot',
+    'Tomato Early Blight',
+    'Tomato Late Blight',
+    'Tomato Leaf Mold',
+    'Tomato Septoria Leaf Spot',
+    'Tomato Spider Mites',
+    'Tomato Target Spot',
+    'Tomato Curl Virus',
+    'Tomato Mosaic Virus',
+    'Tomato_healthy'
+]
